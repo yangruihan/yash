2cb85f5d72adf2336c3f92d3e97110da01a6b39e
C.y <yangruihan@vip.qq.com>
2017-02-21 18:15:12 +0800

    更新alias命令，其语法为alias l=ls

--- builtin_func/alias.py
+++ builtin_func/alias.py
@@ -19,20 +19,24 @@ class AliasFunc(BuiltinFunc):
         BuiltinFunc.__init__(self, 'alias')
 
     def execute(self, cmd):
-        if len(cmd.cmd_simple_args) <= 1:
+        if len(cmd.cmd_simple_args) <= 0 or len(cmd.cmd_simple_args) > 1:
             print("alias: 命令参数个数错误")
             return ShellStatus.RUN
 
-        cmd1 = cmd.cmd_simple_args[0]
-        temp_cmd = cmd.raw_cmd[cmd.raw_cmd.index('alias') + len('alias'):].strip()
-        cmd2 = temp_cmd[temp_cmd.index(cmd1) + len(cmd1):].strip()
+        arg = cmd.cmd_simple_args[0].split('=')
+        alias_cmd = arg[0]
+
+        if '"' in cmd.raw_cmd:
+            raw_cmd = cmd.raw_cmd[cmd.raw_cmd.index('"') + 1: cmd.raw_cmd.rfind('"')]
+        else:
+            raw_cmd = arg[1]
 
         # 如果有 w 选项则将别名写入.yashrc
         if 'w' in cmd.cmd_options:
-            content = '\nalias %s="%s"\n' % (cmd1, cmd2)
+            content = '\nalias %s="%s"\n' % (alias_cmd, raw_cmd)
             FileHelper.write_file_from_string(cmd.env['runtime_config_file_name'], content, 'w+')
 
-        cmd.env['alias'][cmd1] = cmd2
+        cmd.env['alias'][alias_cmd] = raw_cmd
 
         return ShellStatus.RUN
 
